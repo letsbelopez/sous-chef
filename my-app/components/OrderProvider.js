923b3f2f95abdb074ddd5620856a92c0d57b9571
letsbelopez <letsbelopez@gmail.com>
2018-11-25 09:40:42 -0800

    starting to integrate ingredients totalling from metafields.

--- my-app/components/OrderProvider.js
+++ my-app/components/OrderProvider.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 const OrderContext = React.createContext();
 
 class OrderProvider extends Component {
   state = {
-    path: '',
+    path: "",
     dates: null,
     orders: [],
     requestInProgress: false
-  }
+  };
 
-  buildOrderRows = (orders) => {
+  buildIngredientRows = () => {
+    const orders = this.state.orders;
+    const promises = [];
+
+    orders.forEach(row => {
+      const p = this.getProductMetafields(row[1]);
+      promises.push(p);
+    });
+
+    // TODO Finish ingredient totalling
+    Promise.all(promises)
+      .then(metafields => {
+        console.log(metafields);
+        metafields.forEach(metafield => {
+          if (metafield) {
+            const ingredients = JSON.parse(metafield[0].value);
+            console.log(ingredients);
+          }
+        });
+      })
+      .catch(error => console.error(error));
+  };
+
+  buildOrderRows = orders => {
     const productQuantities = orders
       .reduce((obj, order) => {
         return [...obj, ...order.line_items];
@@ -26,67 +49,96 @@ class OrderProvider extends Component {
         return obj;
       }, {});
 
-    const rows = Object.keys(productQuantities).map((key) => {
+    const rows = Object.keys(productQuantities).map(key => {
       return [
         key,
         productQuantities[key].product_id,
-        productQuantities[key].quantity,
+        productQuantities[key].quantity
       ];
     });
 
-    this.setState({orders: rows, requestInProgress: false});
-  }
+    this.setState({ orders: rows, requestInProgress: false });
+    this.buildIngredientRows();
+  };
 
   getOrders = () => {
-    this.setState({requestInProgress: true});
+    this.setState({ requestInProgress: true });
 
     const fetchOptions = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json"
       },
-      credentials: 'include',
-    }
+      credentials: "include"
+    };
 
     fetch(`/api${this.state.path}`, fetchOptions)
-      .then((response) => response.json())
-      .then((json) => {
-        this.buildOrderRows(json.orders)
+      .then(response => response.json())
+      .then(json => {
+        this.buildOrderRows(json.orders);
       })
-      .catch((error) => {
-        console.error(error)
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  getProductMetafields = id => {
+    this.setState({ requestInProgress: true });
+
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      credentials: "include"
+    };
+
+    return fetch(`/api/products/${id}/metafields.json`, fetchOptions)
+      .then(response => response.json())
+      .then(json => {
+        const ingredients = json.metafields.filter(metafield => {
+          return metafield.namespace === "sousChef";
+        });
+        if (ingredients.length > 0) {
+          return ingredients;
+        }
       })
-  }
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-  updatePath = (dates) => {
-    const path = `/orders.json?processed_at_min=${dates.start.toISOString()}`
-    this.setState({ path })
-  }
+  updatePath = dates => {
+    const path = `/orders.json?processed_at_min=${dates.start.toISOString()}`;
+    this.setState({ path });
+  };
 
-  updateDates = (dates) => {
-    this.setState({ dates })
-  }
+  updateDates = dates => {
+    this.setState({ dates });
+  };
 
   render() {
     return (
-      <OrderContext.Provider 
+      <OrderContext.Provider
         value={{
           path: this.state.path,
           dates: this.state.dates,
           orders: this.state.orders,
           getOrders: this.getOrders,
+          getProductMetafields: this.getProductMetafields,
           updateDates: this.updateDates,
           updatePath: this.updatePath
         }}
       >
         {this.props.children}
       </OrderContext.Provider>
-    )
+    );
   }
 }
 
-const OrderConsumer = OrderContext.Consumer
+const OrderConsumer = OrderContext.Consumer;
 
-export default OrderProvider
-export { OrderConsumer }
+export default OrderProvider;
+export { OrderConsumer };
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,25 +1,66 @@
-import React, { Component } from 'react'
-import { Card, Page, Layout } from '@shopify/polaris'
-import DatePopover from '../components/DatePopover'
-import OrderTotalsTable from '../components/OrderTotalsTable'
-import { OrderConsumer } from '../components/OrderProvider'
-
+import React, { Component } from "react";
+import { Card, Layout, Page, Tabs } from "@shopify/polaris";
+import DatePopover from "../components/DatePopover";
+import OrderTotalsTable from "../components/OrderTotalsTable";
+import IngredientTotalsTable from "../components/IngredientTotalsTable";
+import { OrderConsumer } from "../components/OrderProvider";
 
 class App extends Component {
+  state = {
+    selected: 0,
+    tabs: [
+      {
+        id: "order-totals",
+        content: "Order totals",
+        accessibilityLabel: "Order totals",
+        panelID: "order-totals"
+      },
+      {
+        id: "ingredient-totals",
+        content: "Ingredient totals",
+        accessibilityLabel: "Ingredient totals",
+        panelID: "ingredient-totals"
+      }
+    ]
+  };
+
+  handleTabChange = selectedTabIndex => {
+    this.setState({ selected: selectedTabIndex });
+  };
+
+  renderTabContent = (orders, requestInProgress) => {
+    const selected = this.state.selected;
+    if (selected === 0) {
+      return (
+        <OrderTotalsTable
+          orders={orders}
+          requestInProgress={requestInProgress}
+        />
+      );
+    } else {
+      return (
+        <IngredientTotalsTable
+          orders={orders}
+          requestInProgress={requestInProgress}
+        />
+      );
+    }
+  };
 
   render() {
     return (
       <Page title="Meal Totals">
-          <OrderConsumer>
-            {({getOrders, orders, requestInProgress}) => (
-              <Layout>
+        <OrderConsumer>
+          {({ getOrders, orders, requestInProgress }) => (
+            <Layout>
               <Layout.Section>
                 <Card
                   title="Search criteria"
                   primaryFooterAction={{
                     content: "Total meals",
                     onAction: getOrders
-                  }}>
+                  }}
+                >
                   <Card.Section title="Choose date">
                     Select meals ordered from <DatePopover /> to today.{" "}
                   </Card.Section>
@@ -27,15 +68,21 @@ class App extends Component {
               </Layout.Section>
               <Layout.Section>
                 <Card>
-                  <OrderTotalsTable orders={orders} requestInProgress={requestInProgress} />
+                  <Tabs
+                    tabs={this.state.tabs}
+                    selected={this.state.selected}
+                    onSelect={this.handleTabChange}
+                    fitted={false}
+                  />
+                  {this.renderTabContent(orders, requestInProgress)}
                 </Card>
               </Layout.Section>
             </Layout>
           )}
         </OrderConsumer>
       </Page>
-    )
+    );
   }
 }
 
-export default props => (<App />)
+export default props => <App />;
